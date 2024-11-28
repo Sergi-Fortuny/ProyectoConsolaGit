baed7a3922715d561ad1b77240a0569c102c9ce1
Sergi-Fortuny <serforalb@alu.edu.gva.es>
2024-11-28 21:19:38 +0100

    Añadido MensajeOperaciones.java con métodos de bienvenida y despedida

---
+++ src/MensajeOperaciones.java
@@ -0,0 +1,8 @@
+public class MensajeOperaciones {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Bienvenido al programa");
+        System.out.println("Gracias por usar el programa");
+    }
+}
